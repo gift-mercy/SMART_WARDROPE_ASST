5f7a38a80ae3e79e2ddf570ec10e59d7175223a0
Rebecca Nabbanja <nabbanjarebecca9@gmail.com>
2026-07-14 17:23:53 +0300

    Still worked on image preview screen

--- smart_wardrobe_assistant/lib/screens/camera/image_preview_screen.dart
+++ smart_wardrobe_assistant/lib/screens/camera/image_preview_screen.dart
@@ -8,3 +8,17 @@ const ImagePreviewScreen({
 super.key,
 required this.imagePath,
 });
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text("Image Preview"),
+    ),
+    body: Center(
+      child: Image.file(
+        File(imagePath),
+      ),
+    ),
+  );
+}
+}

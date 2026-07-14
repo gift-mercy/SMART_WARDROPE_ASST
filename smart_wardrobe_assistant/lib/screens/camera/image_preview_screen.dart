d9121abd4d919ca3b7f1841759652911d9227e3a
Rebecca Nabbanja <nabbanjarebecca9@gmail.com>
2026-07-14 17:21:23 +0300

    Worked on the image preview screen

---
+++ smart_wardrobe_assistant/lib/screens/camera/image_preview_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ImagePreviewScreen extends StatelessWidget {
+
+final String imagePath;
+
+const ImagePreviewScreen({
+super.key,
+required this.imagePath,
+});
